13c68208e6f1f96f517848788262d0d838f92c86
chajr <chajr@bluetree.pl>
2018-02-01 17:45:11 +0100

    Added some empty classes for future usage

---
+++ src/Directory.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CloverReporter;
+
+class Directory
+{
+    public function __construct($files)
+    {
+        var_dump($files);
+    }
+}
---
+++ src/Render.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace CloverReporter;
+
+class Render
+{
+    public function __construct($options, $infoList)
+    {
+        var_dump($options);
+    }
+}
